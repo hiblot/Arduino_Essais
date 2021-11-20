b171436b861d1b8a80a763544c7a24537b93c2e1
Nicolas HIBLOT <nicolas@hiblot.com>
2021-11-20 21:16:59 +0100

    Mise a jour : Manque juste l'affichage

--- EEPROM_RW_INT/sketch_nov20a/sketch_nov20a.ino
+++ EEPROM_RW_INT/sketch_nov20a/sketch_nov20a.ino
@@ -12,6 +12,8 @@ float valeurINITIALE = 0;
 float valeurCurrente = 0;
 float valeurTOTALEGLOBALE = 0;
 
+float valeurMAX = 50;
+
 float valeurR1;
 float valeurR2; 
 
@@ -20,8 +22,8 @@ void setup() {
   Serial.begin(9600) ;
 
   //first INIT 
-  EEPROM.put(adresseW1, valeurINITIALE);
-  EEPROM.put(adresseW2, valeurINITIALE);
+  //EEPROM.put(adresseW1, valeurINITIALE);
+  //EEPROM.put(adresseW2, valeurINITIALE);
   
   EEPROM.get(adresseW1, valeurR1);  // Retourne la valeur FLOAT contenue à l’adresse « 42 » de l’eeprom
   EEPROM.get(adresseW2, valeurR2);  // Retourne la valeur FLOAT contenue à l’adresse « 142 » de l’eeprom
@@ -33,5 +35,33 @@ void setup() {
 void loop() 
 {
 
+  // Tester 
+  increment();
+
   
+}
+
+void increment()
+{
+    EEPROM.get(adresseW1, valeurR1);  // Retourne la valeur FLOAT contenue à l’adresse « 42 » de l’eeprom
+    EEPROM.get(adresseW2, valeurR2);  // Retourne la valeur FLOAT contenue à l’adresse « 142 » de l’eeprom
+    
+
+
+    valeurR1 = valeurR1 + 1;
+    valeurR2 = valeurR2 + 1;
+    if (valeurR1 > valeurMAX)
+      {
+        valeurR1 = 0;
+      }
+
+    EEPROM.put(adresseW1, valeurR1);
+    EEPROM.put(adresseW2, valeurR2);
+  
+  
+    Serial.print("Valeur à l’adresse "); Serial.print(adresseW1);  Serial.print(" = "); Serial.println(valeurR1); 
+    Serial.print("Valeur à l’adresse "); Serial.print(adresseW2);  Serial.print(" = "); Serial.println(valeurR2); 
+
+    delay(2500);
+
 }
