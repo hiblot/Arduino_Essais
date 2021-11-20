af1b9f76406e25a10290b4b44439d6c8bc26e9ea
Nicolas HIBLOT <nicolas@hiblot.com>
2021-11-20 19:01:04 +0100

    Gesstion des eeprom

---
+++ EEPROM_RW_INT/sketch_nov20a/sketch_nov20a.ino
@@ -0,0 +1,37 @@
+// Exemple pour lire la valeur contenue à l’adresse 14 de l’EEPROM de son Arduino
+// avec la fonction EEPROM.read
+#include <EEPROM.h>
+
+int adresseW1 = 42;
+float valeurW1 = 17777;
+
+int adresseW2 = 142;
+float valeurW2 = 696969;
+
+float valeurINITIALE = 0;
+float valeurCurrente = 0;
+float valeurTOTALEGLOBALE = 0;
+
+float valeurR1;
+float valeurR2; 
+
+
+void setup() {
+  Serial.begin(9600) ;
+
+  //first INIT 
+  EEPROM.put(adresseW1, valeurINITIALE);
+  EEPROM.put(adresseW2, valeurINITIALE);
+  
+  EEPROM.get(adresseW1, valeurR1);  // Retourne la valeur FLOAT contenue à l’adresse « 42 » de l’eeprom
+  EEPROM.get(adresseW2, valeurR2);  // Retourne la valeur FLOAT contenue à l’adresse « 142 » de l’eeprom
+  
+  Serial.print("Valeur à l’adresse "); Serial.print(adresseW1);  Serial.print(" = "); Serial.println(valeurR1); 
+  Serial.print("Valeur à l’adresse "); Serial.print(adresseW2);  Serial.print(" = "); Serial.println(valeurR2); 
+}
+
+void loop() 
+{
+
+  
+}
